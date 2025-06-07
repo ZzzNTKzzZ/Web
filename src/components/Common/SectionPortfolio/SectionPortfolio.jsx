18901bf164e31d18158e98a9e24b9a3e576d1ac0
ZzzNTKzzZ <ntk22052006@gmail.com>
2025-06-08 01:46:12 +0700

    Done edit navbar 2

--- src/components/Common/SectionPortfolio/SectionPortfolio.jsx
+++ src/components/Common/SectionPortfolio/SectionPortfolio.jsx
@@ -1,31 +1,42 @@
-import { useRef, useState, forwardRef } from "react";
+import { useRef, forwardRef } from "react";
 import style from "./SectionPortfolio.module.css";
 
 import { ReactComponent as SettingIcon } from "../../../assets/icon/Setting.svg";
+import { ReactComponent as TrashIcon } from "../../../assets/icon/Trash.svg";
 
 const SectionPortfolio = forwardRef(function SectionPortfolio(props, ref) {
   const nodeRef = useRef(null);
 
   const handleSettingsClick = () => {
     props.setEditMenu(props.sectionKey);
-    props.setIsActive();  // Notify parent this section is active
+    props.setIsActive(); // Notify parent this section is active
   };
 
   return (
     <div
       ref={ref || nodeRef}
       className={`${props.active ? style.active : ""} ${style.sectionPortfolio}`}
-      style={{backgroundColor: props.backgroundColor}}
+      style={{ backgroundColor: props.backgroundColor }}
+      onClick={props.setIsActive}
     >
       {props.children}
       <div className={style.settingMenu}>
-        <button onClick={handleSettingsClick}>
+        <button onClick={(e) => {
+          e.stopPropagation();
+          handleSettingsClick();
+        }}>
           <SettingIcon />
+        </button>
+
+        <button onClick={(e) => {
+          e.stopPropagation();
+          props.handleDelete();
+        }}>
+          <TrashIcon />
         </button>
       </div>
     </div>
   );
 });
-
 
 export default SectionPortfolio;
