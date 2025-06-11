d098ed160c1887d5250180713fa987970a8a9616
TheAnh1404 <anhnt.24ite@vku.udn.vn>
2025-06-11 18:10:47 +0700

    Contact but not done yet

---
+++ src/Page/Contact/Contact.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+
+import Navbar from "../../components/Navbar/Navbar";
+import HeroSection from "../../components/Contact/HeroSection/HeroSection";
+import Bar from "../../components/Contact/Bar/Bar";
+import Footer from "../../components/Footer/Footer";
+
+function Contact() {
+    return (
+        <div className="contact">
+            <Navbar />
+            <HeroSection />
+            <Bar/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Contact;
