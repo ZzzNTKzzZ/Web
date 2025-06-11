2ddeb58e1fc545fa6a2ec43e5bdb12d32a8ebe8d
TheAnh1404 <anhnt.24ite@vku.udn.vn>
2025-06-11 16:32:04 +0700

    add image for templateGrid

---
+++ src/components/Portfolio/Template/TemplateGrid/TemplateGridImg.jsx
@@ -0,0 +1,26 @@
+import template1 from "../../../../assets/Img/TemplateGrid/template1.png";
+import template2 from "../../../../assets/Img/TemplateGrid/template2.jpg";
+import template3 from "../../../../assets/Img/TemplateGrid/template3.webp";
+import template4 from "../../../../assets/Img/TemplateGrid/template4.jpg";
+import template5 from "../../../../assets/Img/TemplateGrid/template5.jpg";
+import template6 from "../../../../assets/Img/TemplateGrid/template6.jpg";
+import template7 from "../../../../assets/Img/TemplateGrid/template7.webp";
+import template8 from "../../../../assets/Img/TemplateGrid/template8.jpg";
+import template9 from "../../../../assets/Img/TemplateGrid/template9.jpg";
+import template10 from "../../../../assets/Img/TemplateGrid/template10.jpg";
+import template11 from "../../../../assets/Img/TemplateGrid/template11.jpg";
+import template12 from "../../../../assets/Img/TemplateGrid/template12.jpg";
+import template13 from "../../../../assets/Img/TemplateGrid/template13.webp";
+import template14 from "../../../../assets/Img/TemplateGrid/template14.jpg";
+import template15 from "../../../../assets/Img/TemplateGrid/template15.webp";
+import template16 from "../../../../assets/Img/TemplateGrid/template16.png";
+import template17 from "../../../../assets/Img/TemplateGrid/template17.png";
+import template18 from "../../../../assets/Img/TemplateGrid/template18.jpg";
+
+const TemplateGridImg = [
+  template1, template2, template3, template4, template5, template6,
+  template7, template8, template9, template10, template11, template12,
+  template13, template14, template15, template16, template17, template18
+];
+
+export default TemplateGridImg;
