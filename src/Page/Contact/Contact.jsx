32a499e9017c77bd4a88626c98c64facfe0a9c60
TheAnh1404 <anhnt.24ite@vku.udn.vn>
2025-06-11 22:34:54 +0700

    add TeamSection to Contact

--- src/Page/Contact/Contact.jsx
+++ src/Page/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import react from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import HeroSection from "../../components/Contact/HeroSection/HeroSection";
 import Bar from "../../components/Contact/Bar/Bar";
+import TeamSection from "../../components/Contact/TeamSection/TeamSection";
 import Footer from "../../components/Footer/Footer";
 
 function Contact() {
@@ -11,6 +12,7 @@ function Contact() {
             <Navbar />
             <HeroSection />
             <Bar/>
+            <TeamSection/>
             <Footer/>
         </div>
     )
