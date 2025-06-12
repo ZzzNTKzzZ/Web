ac6ee7e768be40d6553f14ddb59443478814e7b4
TheAnh1404 <anhnt.24ite@vku.udn.vn>
2025-06-12 23:03:19 +0700

    add ValueSection

--- src/Page/Contact/Contact.jsx
+++ src/Page/Contact/Contact.jsx
@@ -5,6 +5,7 @@ import HeroSection from "../../components/Contact/HeroSection/HeroSection";
 import Bar from "../../components/Contact/Bar/Bar";
 import HighlightSection from "../../components/Contact/HighlightSection/HighlightSection";
 import TeamSection from "../../components/Contact/TeamSection/TeamSection";
+import ValueSection from "../../components/Contact/ValueSection/ValueSection";
 import Footer from "../../components/Footer/Footer";
 
 function Contact() {
@@ -15,6 +16,7 @@ function Contact() {
             <Bar/>
             <HighlightSection />
             <TeamSection/>
+            <ValueSection/>
             <Footer/>
         </div>
     )
