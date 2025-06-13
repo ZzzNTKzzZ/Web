d16cfb5b98cb8f161accc78d6c5f1885f013b28c
TheAnh1404 <anhnt.24ite@vku.udn.vn>
2025-06-14 01:17:04 +0700

    add reponsive to Portfolio

--- src/Page/Portfolio/PortfolioHome.jsx
+++ src/Page/Portfolio/PortfolioHome.jsx
@@ -5,9 +5,25 @@ import HeroSection from "../../components/Portfolio/HeroSection/HeroSection";
 import Footer from "../../components/Footer/Footer";
 import TemplatePage from "../../components/Portfolio/TemplatePage/TemplatePage";
 
+import useScrollDirection from "../../Hooks/useScrollY";
+import useScreenWidth from "../../Hooks/useScreenWidth";
+
+import NavbarResponsive from "../../components/Navbar/NavbarResponsive";
+
 function PortfolioHome(){
+    const scrollDirection = useScrollDirection();
+    const currentScreenWidth = useScreenWidth();
+
+    const MOBILE_BREAKPOINT = 768; // px
     return(
         <div>
+            {currentScreenWidth < MOBILE_BREAKPOINT ? (
+        // Nếu chiều rộng màn hình nhỏ hơn 768px, hiển thị NavbarResponsive
+        <NavbarResponsive/>
+      ) : (
+        // Nếu chiều rộng màn hình lớn hơn hoặc bằng 768px, hiển thị Navbar
+        <Navbar scrollDirection={scrollDirection} />
+      )}
             <Navbar/>
             <HeroSection/>
             <TemplatePage/>
