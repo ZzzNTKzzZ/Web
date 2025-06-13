91ca9fd01ccd5ab5b9a0cfa61f2578b234272b8b
TheAnh1404 <anhnt.24ite@vku.udn.vn>
2025-06-13 23:15:49 +0700

    add animate

--- src/components/Portfolio/Template/TemplatePage/TemplatePage.jsx
+++ src/components/Portfolio/Template/TemplatePage/TemplatePage.jsx
@@ -11,8 +11,10 @@ function TemplatePage() {
     return (
         <div className={style.templatePage}>
             {/* <Sidebar /> */}
-            <div className={style.mainContent}>
-                <h2 className={style.title}>Quickly create websites from professional templates</h2>
+            <div className={style.mainContent} id="services">
+                <h2 className={style.title}>
+                    Quickly create websites from professional templates
+                </h2>
                 <TemplateGrid page={currentPage} />
                 <Pagination
                     currentPage={currentPage}
